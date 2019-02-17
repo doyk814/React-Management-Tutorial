76960dfd4375dcabf0f6e43a1534388d79118681
Doyeon Kim <doyk814@naver.com>
2019-02-17 19:36:29 +0900

    Create React Project #2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+const customer = {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/any',
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2>
-      </div>
+      <Customer 
+        id = { customer.id }
+        image = { customer.image }
+        name = { customer.name }
+        birthday = { customer.birthday }
+        gender = { customer.gender }
+        job = { customer.job }
+      />
     );
   }
 }
